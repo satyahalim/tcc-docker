329a4e93f9fd47fee37bbe087c582bbffc3d03a0
Satyagraha Halim Widiatmojo <160807125+satyahalim@users.noreply.github.com>
2025-03-25 16:48:39 +0700

    Update database.js

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 //nama db, nama user, password
-const db = new Sequelize("tcc","root","",{
+const db = new Sequelize("tcc","root","meong123",{
     host: "34.60.58.134",
     dialect: "mysql"
 })
